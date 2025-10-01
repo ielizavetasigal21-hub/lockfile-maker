fd8b43ca22b8acbff8fcfeead35c75fb6dddb8c2
ielizavetasigal21-hub <ielizaveta.sigal21@gmail.com>
2025-10-01 13:28:56 +0000

    Serve homepage at /; remove redirect to /home

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: true,
-      },
-    ]
-  },
-}
+  reactStrictMode: true,
+  // no redirects – serve the homepage at /
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
